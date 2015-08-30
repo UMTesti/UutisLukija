04c835837080d6c2723cc5f17b399a6d1304c23f
Pihla Toivanen <toivanenpihla@gmail.com>
2015-08-30 18:38:01 +0300

    nimi

--- before_install.sh
+++ before_install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/hy-ohtu/P-iv-n-Uutiset-.git
-mvn clean install -U -f P-iv-n-Uutiset-.git
+git clone https://github.com/hy-ohtu/PaivanUutiset.git
+mvn clean install -U -f PaivanUutiset.git
 git clone https://github.com/hy-ohtu/HackerNewsUutiset.git
 mvn clean install -U -f HackerNewsUutiset.git
